50043f08c5cb406ed296e0e0bade0f79c742e31d
Alumnos Santiago Centro <alumnos09@inacap.cl>
2024-11-19 20:38:06 -0300

    empezando el proyecto

---
+++ src/app/componentes/layout/NavBarApp.jsx
@@ -0,0 +1,34 @@
+// src/app/components/common/NavBarApp.js
+
+import Link from 'next/link';
+
+const NavBarApp = () => {
+  return (
+    <nav className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <Link href="/" className="text-xl font-bold">
+          BikeWorkshops
+        </Link>
+
+        {/* Links de navegación */}
+        <div className="space-x-4">
+          <Link href="/" className="hover:underline">
+            Inicio
+          </Link>
+          <Link href="/workshops" className="hover:underline">
+            Talleres
+          </Link>
+          <Link href="/profile" className="hover:underline">
+            Perfil
+          </Link>
+          <Link href="/login" className="hover:underline">
+            Iniciar Sesión
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBarApp;
